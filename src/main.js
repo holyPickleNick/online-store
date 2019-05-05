e5bdb84d11aca265b43a5bca269e717bc74ce7ed
Josh Lalonde <mori.ryuuji3@gmail.com>
2019-05-05 03:44:37 +0000

    Switch package manager, install bootstrap

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueI18n from "vue-i18n";
 import VueRouter from "vue-router";
+import BootstrapVue from "bootstrap-vue";
 import { setupMessages } from "./locale";
 import { router } from "./router";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(VueI18n);
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
