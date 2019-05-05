7651785de51f58801d3a1949a89854b6db629412
Josh Lalonde <mori.ryuuji3@gmail.com>
2019-05-05 21:59:57 +0000

    Setup vuex

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@ import App from "./App.vue";
 import VueI18n from "vue-i18n";
 import VueRouter from "vue-router";
 import BootstrapVue from "bootstrap-vue";
+import Vuex from "vuex";
 import { setupMessages } from "./locale";
 import { router } from "./router";
+import { store } from "./store";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(VueI18n);
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
@@ -19,5 +22,6 @@ const i18n = setupMessages();
 new Vue({
   i18n,
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import Vuex from "vuex";
+
+export const store = new Vuex.Store({
+
+});
